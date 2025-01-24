941e401b19fbe947a7c2de1af7219ab77c1a3f66
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2025-01-24 10:45:01 +0100

    fixed it

--- fibonnaci/src/main.rs
+++ fibonnaci/src/main.rs
@@ -1,16 +1,17 @@
 fn main() {
     println!("Hello, world!");
-    let no = 5;
+    let no = 7;
     let x = generate_fibonnaci(no);
     println!("Fibonnaci of {no} is {x}");
 }
 
 fn generate_fibonnaci(x: u32) -> u32{
-  if x == 0 {
+  if x == 1 {
     0
-  } else if x == 1 {
+  } else if x == 2  {
     1
   } else {
-    x + generate_fibonnaci(x-1)
+    // prev last no of x + last no of x
+   generate_fibonnaci(x-2) + generate_fibonnaci(x-1)
   }
 }
