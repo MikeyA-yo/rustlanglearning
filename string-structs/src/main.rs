507c86208eec92a888e0631d0a2b4801ea2b5b2f
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2025-01-26 21:45:36 +0100

    idk if i am doing borrow checking right

--- string-structs/src/main.rs
+++ string-structs/src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     println!("The new user is: {user:?}");
     let user2 = new_user(String::from("Rapto"), user.age);
     println!("The last user is: {user2:?}");
+    let subname = &user.name[0..user.name.len()/2];
+    println!("{}", subname);
 }
 
 
