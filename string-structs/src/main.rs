15b00279a013c489f41e979192ddb10e7c3d9bf0
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2025-01-26 19:24:41 +0100

    add a basic struct field

--- string-structs/src/main.rs
+++ string-structs/src/main.rs
@@ -1,9 +1,21 @@
+#[derive(Debug)]
+struct User {
+    name : String,
+    age: u16
+}
 fn main() {
-    let mut name = String::from("Ayomide Oluwatola");
-    let user = string_ref(&mut name);
-    println!("{user}");
+    let name = String::from("Ayomide Oluwatola");
+    let age = 17;
+    let user = new_user(name, age);
+    println!("The new user is: {user:?}");
+    let user2 = new_user(String::from("Rapto"), user.age);
+    println!("The last user is: {user2:?}");
 }
 
-fn string_ref(s: &mut String) -> &String{
-    s
+
+fn new_user(name:String, age:u16) -> User {
+  User {
+    name,
+    age
+  }
 }
