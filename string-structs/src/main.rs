ff747da779b7fe72710f9244625c9c75d544dfec
Oluwatola Ayomide <ayomideoluwatola1@gmail.com>
2025-02-02 16:43:55 +0100

    added method to the structs

--- string-structs/src/main.rs
+++ string-structs/src/main.rs
@@ -3,6 +3,14 @@ struct User {
     name : String,
     age: u16
 }
+
+impl User {
+  fn format(&self) -> String{
+    let age_str = &self.age.to_string();
+    let  fmt = self.name.clone()+ &String::from(" ") + age_str;
+    fmt
+  }
+}
 fn main() {
     let name = String::from("Ayomide Oluwatola");
     let age = 17;
@@ -12,6 +20,8 @@ fn main() {
     println!("The last user is: {user2:?}");
     let subname = &user.name[0..user.name.len()/2];
     println!("{}", subname);
+    println!("User format: {}", user.format());
+    println!("User format: {}", user2.format());
 }
 
 
